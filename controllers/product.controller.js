a3df2db959cbb0b409ba74563bcbad60e0526b38
ubong <ubongnt@gmail.com>
2024-02-16 01:32:58 +0100

    single product

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -9,7 +9,7 @@ export const createProduct = async (req, res) => {
   }
 };
 
-export const getProduct = async (req, res) => {
+export const getProducts = async (req, res) => {
   try {
     const products = await Product.find({});
     res.status(200).json(products);
@@ -17,3 +17,15 @@ export const getProduct = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const getProduct = async (req, res) => {
+    try {
+
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+        
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
--- routes/product.route.js
+++ routes/product.route.js
@@ -1,11 +1,12 @@
 import express  from "express";
 
-import { createProduct, getProduct } from "../controllers/product.controller.js";
+import { createProduct, getProduct, getProducts } from "../controllers/product.controller.js";
 
 const router = express.Router();
 
 router.post('/', createProduct);
-router.get('/', getProduct);
+router.get('/', getProducts);
+router.get('/:id', getProduct)
 
 
 export default router
