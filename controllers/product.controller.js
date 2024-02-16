e0065dd0764892498d53b479e22f6f5b2210d88b
ubong <ubongnt@gmail.com>
2024-02-16 01:10:38 +0100

    restructure the files

---
+++ controllers/product.controller.js
@@ -0,0 +1,10 @@
+import Product from "../model/product.model.js";
+
+export const createProduct = async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import Product from "./model/product.model.js";
+import productRoute from "./routes/product.route.js";
 
 dotenv.config();
 const app = express();
 
+// middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routes
+app.use("/api/products", productRoute);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the CRUD with Node.js Express and MongoDB");
+});
 
 mongoose
   .connect(process.env.MONGO)
@@ -19,16 +28,3 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
-app.post("/api/products", async (req, res) => {
-  try {
-    const product = await Product.create(req.body);
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.get("/", (req, res) => {
-  res.send("Welcome to the CRUD with Node.js Express and MongoDB");
-});
